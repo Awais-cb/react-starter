086e67257802cc8a07b7ff6a213bbb6e55427ab3
Awais Fiaz <awais.fiaz@oladoc.com>
2020-10-26 12:46:32 +0500

    REDUX added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,44 @@ import './global/scss/index.scss';
 // Importing component to use
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// REDUX START
+// STORE -> GLOBALIZED STATE
+// ACTION -> EVENTS TO MODIFY STORE U DESCRIBE WHAT U WANNA DO (E.G I AM HUNGRY) (INCREMENT, DECREMENT)
+const increment = () => {
+  return {
+    type : 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type : 'DECREMENT'
+  }
+}
+
+// REDUCER -> TAKES ACTION TO STORE (we can have multiple reducers)
+const counter = (state = 0 , action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+    return state + 1
+    break;
+  case 'DECREMENT':
+    return state - 1
+    break;
+  }
+}
+let store = createStore(counter)
+    
+// DISPLAY CONSOLE
+store.subscribe(() => { console.log('State Management ', store.getState() ) })
+
+// DISPATCH -> EXECUTES THE ACTION
+store.dispatch(increment());
+store.dispatch(decrement());
+// REDUX END
+
+
 
 ReactDOM.render(
   <React.StrictMode>
